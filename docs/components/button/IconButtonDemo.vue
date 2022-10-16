a145de28aa7da6e6b1308473ad89bd9487700ab1
cullyfung <fcx.cli@qq.com>
2022-10-17 00:07:46 +0800

    feat: add button docs

---
+++ docs/components/button/IconButtonDemo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-row">
+    <w-button icon="edit" plain></w-button>
+    <w-button icon="delete" plain></w-button>
+    <w-button icon="share" plain></w-button>
+    <w-button round plain icon="search">搜索</w-button>
+  </div>
+</template>
