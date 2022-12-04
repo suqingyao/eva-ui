9fc4fd0ce40566d91486940f4081d146fb28a237
cullyfung <cullyfung@gmail.com>
2022-12-04 16:11:00 +0800

    chore: update prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,6 @@
 module.exports = {
-  useTab: false,
-  tabWidth: 2,
   semi: false,
   singleQuote: true,
   trailingComma: 'none',
-  arrowParens: 'avoid',
-  printWidth: 80
+  arrowParens: 'avoid'
 }
